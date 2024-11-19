e6b09ff9bab383cb53d9b71fc54f2841246a6f2f
Jwise Devs <jwisedevs@gmail.com>
2024-11-19 13:52:04 +0200

    teste do backend

---
+++ routes.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const route = express.Router();
+const homeController = require('./src/controllers/HomeController')
+
+route.get('/',homeController.index);
+
+module.exports = route;
